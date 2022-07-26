4e9c3bb45e0bb295ee63681c2b7e09f697f1364b
Abagna <abagnasalifusasco@gmail.com>
2022-07-26 16:17:03 +0000

    latest commit

--- app/screens/ResponseScanner.js
+++ app/screens/ResponseScanner.js
@@ -6,29 +6,31 @@ import axios from "axios";
 function ResponseScanner({ route }) {
   const resData = route.params;
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState("");
+  const [readData, setReadData] = useState("");
 
   useEffect(() => {
-    const readData = async () => {
-      var bodyFormData = new FormData();
-      bodyFormData.append("resData", resData);
-      const { data } = await axios({
-        method: "post",
-        url: "https://beta.kpododo.com/api/v1/qr-scan-read.php",
-        data: bodyFormData,
-        headers: { "Content-Type": "multipart/form-data" },
-      })
-        .then(function (response) {
-          //handle success
-          console.log(response);
-        })
-        .catch(function (response) {
-          //handle error
-          console.log(response);
-        });
-    };
-    readData();
+    handleReadData();
   }, []);
+
+  const handleReadData = async () => {
+    var bodyFormData = new FormData();
+    bodyFormData.append("resData", resData);
+    const { data } = await axios({
+      method: "post",
+      url: "https://beta.kpododo.com/api/v1/qr-scan-read.php",
+      data: bodyFormData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        //handle success
+        setReadData(response);
+        console.log(response);
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });
+  };
 
   return (
     <View style={styles.container}>
